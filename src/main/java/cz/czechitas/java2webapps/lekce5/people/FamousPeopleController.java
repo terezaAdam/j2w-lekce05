bf3af43e4c82749ab1ae5e1ab4be0aab6567400e
terezaAdam <104910160+terezaAdam@users.noreply.github.com>
2022-10-18 08:54:01 +0200

    ...

--- src/main/java/cz/czechitas/java2webapps/lekce5/people/FamousPeopleController.java
+++ src/main/java/cz/czechitas/java2webapps/lekce5/people/FamousPeopleController.java
@@ -28,4 +28,28 @@ public class FamousPeopleController {
     return result;
   }
 
+  @GetMapping(path = "/", params = "query")
+  public ModelAndView search(String query) {
+    ModelAndView result = new ModelAndView("index");
+    result.addObject("people", service.getByName(query));
+    result.addObject("gender", Gender.values());
+    result.addObject("query", query);
+    return result;
+  }
+  @PostMapping(path = "/delete", params = "id")
+  public String delete (int id) {
+    service.deleteById(id);
+    return "redirect:/";
+  }
+
+  @PostMapping(path = "/")
+  public String append (Person person) {
+    service.append(person);
+    return "redirect:/";
+  }
+  @GetMapping("/test")
+  public String redirect() {
+    return "redirect:/test-po-presmerovani";
+  }
 }
+
